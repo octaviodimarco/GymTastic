0e1281cd95e69f614c0f9ada3326ffe9b3681c6b
octaviodimarco <1610251@ucc.edu.ar>
2019-04-09 17:10:24 -0300

    Initial commit

---
+++ lib/Pages/Setup/signIn.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gymtastic/Pages/home.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if(input.isEmpty){
+                    return 'Provide an email';
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'Email'
+                ),
+                onSaved: (input) => _email = input,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if(input.length < 6){
+                    return 'Longer password please';
+                  }
+                },
+                decoration: InputDecoration(
+                    labelText: 'Password'
+                ),
+                onSaved: (input) => _password = input,
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: signIn,
+                child: Text('Sign in'),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+
+
+  void signIn() async {
+    final formState = _formKey.currentState;
+    if(formState.validate()){
+      formState.save();
+      try{
+        FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+        Navigator.push(context, MaterialPageRoute(builder: (context) => Home(user: user)));
+        //TODO: Navigate to home
+
+      } catch (e){
+        print(e.message);
+
+    }
+
+  }
+
+
+}}
---
+++ lib/Pages/Setup/welcome.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:gymtastic/Pages/Setup/signIn.dart';
+import 'package:gymtastic/Pages/Setup/singUp.dart';
+
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina de inicio')
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: navigateToSignIn,
+            child: Text('Sign in'),
+          ),
+          RaisedButton(
+            onPressed: navigateToSignUp,
+            child: Text('Sign up'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void navigateToSignIn(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(), fullscreenDialog: true));
+  }
+
+  void navigateToSignUp(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage(), fullscreenDialog: true));
+
+  }
+}
